3936c3e6a6a0492d88d30a3980a8490f64cfffd1
Rhian Davies <rhian@jumpingrivers.com>
2021-12-19 19:15:01 +0000

    Add UCC foggy leven estuary

--- _site/R/paddles_map.R
+++ _site/R/paddles_map.R
@@ -1,31 +1,31 @@
 library("rStrava")
 library("dplyr")
 
-stoken <- httr::config(token = strava_oauth("rStrava",
+stoken = httr::config(token = strava_oauth("rStrava",
                                             app_client_id = Sys.getenv("stravaID"),
                                             app_secret = Sys.getenv("stravaSecret"),
                                             app_scope="activity:read_all"))
 
-my_acts <- get_activity_list(stoken)
+my_acts = get_activity_list(stoken)
 
-acts = 
+acts =
 my_acts %>%
   compile_activities()
 
 gpoly_to_sfpoly = function(gpoly){
-  coords <- googlePolylines::decode(gpoly)
-  sfg <- lapply(coords, function(x) sf::st_linestring(x = as.matrix(x[, c(2, 1)])))
-  sfc <- sf::st_sfc(sfg, crs = 4326)
+  coords = googlePolylines::decode(gpoly)
+  sfg = lapply(coords, function(x) sf::st_linestring(x = as.matrix(x[, c(2, 1)])))
+  sfc = sf::st_sfc(sfg, crs = 4326)
   return(sfc)
 }
 
-paddles = 
+paddles =
   acts %>%
   filter(type == "Kayaking") %>%
   filter(!is.na(map.summary_polyline)) %>%
   mutate(geom = gpoly_to_sfpoly(map.summary_polyline)) %>%
-  select(name, geom) %>%
-  st_as_sf() %>%
-  st_set_crs(4326)
+  select(name, distance, geom) %>%
+  sf::st_as_sf() %>%
+  sf::st_set_crs(4326)
 
-st_write(paddles, "paddles.shp")
+sf::st_write(paddles, "R/paddles.shp")
